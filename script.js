505537790243de343f6b6b3b4fb86d72d676857c
kmalakhova <kgrekhova@gmail.com>
2021-07-31 10:18:09 -0700

    Add basic structure

--- script.js
+++ script.js
@@ -1,9 +1,46 @@
-var alertBox=document.querySelector('.alertBox')
+// const axios = require('axios');
 
-function copyPassword(){
-    var copyPassText = document.getElementById("password");
-    copyPassText.select();
-    copyPassText.setSelectionRange(0,9999);
-    document.execCommand("copy");
-    alertBox.classList.toggle('active');
+// const getResponse = () => {
+// axios.get(`${process.env.HEROKU_URL}`)
+//     .then((response) => {
+//     console.log(response.data);
+//     const hint = response.data['hint']
+//     const password = response.data['password']
+//     })
+//     .catch((error) => {
+//     console.log('error.response.data');
+//     });
+// }
+
+// function myFunction() {
+//     var copyText = document.getElementById("password");
+//     copyText.select();
+//     document.execCommand("copy");
+    
+//     var tooltip = document.getElementById("myTooltip");
+//     tooltip.innerHTML = "👍";
+// }
+
+// function outFunc() {
+//     var tooltip = document.getElementById("myTooltip");
+//     tooltip.innerHTML = "CLICK TO COPY";
+// }
+
+// const registerEventHandlers = () => {
+//     const copyPassText = document.getElementById("password");
+//     copyPassText.addEventListener("copy", copyPassword);
+
+//     const hint = document.getElementById("generate-password-button");
+//     hint.addEventListener("click", getResponse);
+
+//     const password = document.getElementById("generate-password-button");
+//     password.addEventListener("click", getResponse);
+// };
+
+// document.addEventListener("DOMContentLoaded", registerEventHandlers);
+
+
+
+function getPassword() {
+    alert("Test")
 }
