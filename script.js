31658f0651b8663ff1c2c2291c9dc0749cd3dee6
kmalakhova <kgrekhova@gmail.com>
2021-08-02 12:49:19 -0700

    Minor changes

--- script.js
+++ script.js
@@ -53,25 +53,3 @@ const registerEventHandlers = () => {
 document.addEventListener('DOMContentLoaded', registerEventHandlers);
 
 
-
-function makeExpandingArea(container) {
- var area = container.querySelector('textarea');
- var span = container.querySelector('span');
- if (area.addEventListener) {
-   area.addEventListener('passwordInput', function() {
-     span.textContent = area.value;
-   }, false);
-   span.textContent = area.value;
- } else if (area.attachEvent) {
-   // IE8 compatibility
-   area.attachEvent('onpropertychange', function() {
-     span.innerText = area.value;
-   });
-   span.innerText = area.value;
- }
-// Enable extra CSS
-container.className += "active";
-}var areas = document.querySelectorAll('.expandingArea');
-var l = areas.length;while (l--) {
- makeExpandingArea(areas[l]);
-}
